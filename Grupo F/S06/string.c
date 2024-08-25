7a82c10b8f1342c5d93b43c53ed46e3f30ff8e48
Softkitty2028 <138616064+Carles2028@users.noreply.github.com>
2024-08-24 18:29:37 -0600

    Add files via upload

---
+++ Grupo F/S06/string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc string argv[])
+{
+    string nombre = get_string("Introduzca su nombre");
+    printf("tu nombre es:%s \n", nombre);
+    return 0;
+}
+
